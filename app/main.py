bf558733070138354ef8a0fa2f83ea1fdb4edc0a
mitso2005 <gpetrakis343@gmail.com>
2026-01-29 22:41:54 +1100

    got historical cpi

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import json
 from app.services.fx import get_latest_rates, get_supported_currencies, get_historical_rates
-from app.services.cpi import get_cpi_data
+from app.services.cpi import get_cpi_data, get_latest_cpi, get_cpi_historical_av
 
 app = FastAPI()
 
@@ -18,4 +18,6 @@ if __name__ == "__main__":
     # print(json.dumps(get_latest_rates(), indent=2))
     # print(json.dumps(get_supported_currencies(), indent=2))
     # print(json.dumps(get_historical_rates(), indent=2))
-    print(json.dumps(get_cpi_data("US", 2000, 2024), indent=2))
+    # print(json.dumps(get_cpi_data("US", 2000, 2024), indent=2))
+    # print(json.dumps(get_latest_cpi("US"), indent=2))
+    # print(json.dumps(get_cpi_historical("US", 2000, 2024), indent=2))
--- app/services/cpi.py
+++ app/services/cpi.py
@@ -1,13 +1,46 @@
 import requests
 from app.config import WORLD_BANK_API_BASE_URL, WORLD_BANK_API_VERSION
 
-
-def get_cpi_data(country_code: str, start: int, end: int):
+def get_latest_cpi(country_code: str):
     url = f"{WORLD_BANK_API_BASE_URL}{WORLD_BANK_API_VERSION}/country/{country_code}/indicator/FP.CPI.TOTL"
     params = {
-        "date": f"{start}:{end}",
+        "format": "json"
+    }
+    
+    data = requests.get(url, params=params).json()
+
+    latest_entry = data[1][0]
+
+    latest_cpi = latest_entry["value"]
+    # latest_year = latest_entry["date"]
+    
+    return latest_cpi
+
+def get_cpi_data(country_code: str, year_start: int, year_end: int):
+    url = f"{WORLD_BANK_API_BASE_URL}{WORLD_BANK_API_VERSION}/country/{country_code}/indicator/FP.CPI.TOTL"
+    params = {
+        "date": f"{year_start}:{year_end}",
         "format": "json"
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.json()
+
+def get_cpi_historical(country_code: str, year_start: int, year_end: int):
+    list_cpi = []
+    url = f"{WORLD_BANK_API_BASE_URL}{WORLD_BANK_API_VERSION}/country/{country_code}/indicator/FP.CPI.TOTL"
+    params = {
+        "date": f"{year_start}:{year_end}",
+        "format": "json"
+    }
+    
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+    if len(data) > 1 and isinstance(data[1], list):
+        for entry in data[1]:
+            year = entry.get("date")
+            cpi_value = entry.get("value")
+            list_cpi.append({"year": year, "cpi": cpi_value})
+        
+    return list_cpi
